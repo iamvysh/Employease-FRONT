d8f1a624f3524784e929f73c0c6af19696a9a99a
iamvysh <vyshnavthaithottathil@gmail.com>
2023-10-02 11:58:48 +0530

    approved employees pagination

--- src/pages/ApprovedEmployees/Approvedemployyes.jsx
+++ src/pages/ApprovedEmployees/Approvedemployyes.jsx
@@ -5,16 +5,31 @@ import axios from 'axios';
 import Navigation from '../../componets/Navigarion';
 import Sidebar from '../../componets/sidebar/Sidebar';
 import { MDBBtn } from 'mdb-react-ui-kit';
+
+
+
+
+
+
+
 const ApprovedEmployees = () => {
+
+
   const navigate = useNavigate();
+  const [pageNumber, setPageNumber] = useState(0);
+  const [numberOfPages, setNumberOfPages] = useState(0);
+
   const [users, setUsers] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
 
   const GetApprovedEmployees = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/agency/approvedemployees");
+      const response = await axios.get(`http://localhost:3000/agency/approvedemployees?page=${pageNumber}`);
+      console.log(response.data);
       console.log(response.data.Data);
-      setUsers([...response.data.Data]);
+      setUsers([...response.data.Data])
+      setNumberOfPages(response.data.totalPages);
+      ;
     } catch (error) {
       console.log(error);
     }
@@ -22,12 +37,25 @@ const ApprovedEmployees = () => {
 
   useEffect(() => {
     GetApprovedEmployees();
-  }, []);
+  }, [pageNumber]);
+  console.log(numberOfPages);
 
   // Function to filter employees based on the search query
-  const filteredUsers = users.filter((user) => {
-    return user.name.toLowerCase().includes(searchQuery.toLowerCase());
-  });
+  // const filteredUsers = users.filter((user) => {
+  //   return user.name.toLowerCase().includes(searchQuery.toLowerCase());
+  // });
+
+  const pages = new Array(numberOfPages).fill(null).map((v, i) => i);
+
+  console.log(pages);
+
+  const gotoPrevious = () => {
+    setPageNumber(Math.max(0, pageNumber - 1));
+  };
+
+  const gotoNext = () => {
+    setPageNumber(Math.min(numberOfPages - 1, pageNumber + 1));
+  };
 
   return (
     <>
@@ -69,21 +97,33 @@ const ApprovedEmployees = () => {
                 </div>
               </div>
             ))
-          ) : (
-            filteredUsers.map((user) => (
-              <div key={user._id} className='card' style={{ height: "14rem", width: "5rem", margin: "2rem 2rem", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", display: "flex", flexDirection: "row", justifyContent: "space-around" }}  onClick={()=>navigate(`/Approvedsingleemployee/${user._id}`)}>
-                <div className="image_div">
-                  <img src={user.imagefile} alt="Curved Image" style={{ borderRadius: "50%", maxWidth: "160px", maxHeight: "250px", padding: ".4rem", width: "100%", height: "90%", objectFit: "cover" }} />
-                </div>
-                <div className="content_div" style={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
-                  <h3>{user.name}</h3>
-                  <p>{user.phonenumber}</p>
-                  <h6 style={{ color: "violet" }}>{user.Aadharnumber}</h6>
-                </div>
-              </div>
-            ))
-          )}
+          ) :null
+          // : (
+          //   filteredUsers.map((user) => (
+          //     <div key={user._id} className='card' style={{ height: "14rem", width: "5rem", margin: "2rem 2rem", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", display: "flex", flexDirection: "row", justifyContent: "space-around" }}  onClick={()=>navigate(`/Approvedsingleemployee/${user._id}`)}>
+          //       <div className="image_div">
+          //         <img src={user.imagefile} alt="Curved Image" style={{ borderRadius: "50%", maxWidth: "160px", maxHeight: "250px", padding: ".4rem", width: "100%", height: "90%", objectFit: "cover" }} />
+          //       </div>
+          //       <div className="content_div" style={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
+          //         <h3>{user.name}</h3>
+          //         <p>{user.phonenumber}</p>
+          //         <h6 style={{ color: "violet" }}>{user.Aadharnumber}</h6>
+          //       </div>
+          //     </div>
+          //   ))
+          // )
+          }
           </div>  
+          <div style={{display:"flex",justifyContent:"center"}}>
+          <button onClick={gotoPrevious}>Previous</button>
+               {pages.map((pageIndex) => (
+        <button key={pageIndex} onClick={() => setPageNumber(pageIndex)}>
+          {pageIndex + 1}
+        </button>
+      ))}
+            <button onClick={gotoNext}>Next</button>
+
+          </div>
         </div>
       </div>
     </>
